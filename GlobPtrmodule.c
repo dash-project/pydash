8a5ba9911fea48a4f28563fad59e183d52f79b1c
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-05 09:46:43 +0200

    Update GlobPtrmodule.c

--- GlobPtrmodule.c
+++ GlobPtrmodule.c
@@ -13,8 +13,24 @@ GlobPtr_init(PyObject *self, PyObject *args)
 }
 
 
-static PyMethodDef SpamMethods[] = {
+static PyMethodDef GlobPtrMethods[] = {
   {"init", GlobPtr_init, METH_VARARGS, "Create a Global Pointer"},
   {NULL, NULL, 0, NULL}
 };
 
+static struct PyModuleDef GlobPtrmodule = {
+  PyModuleDef_HEAD_INIT,
+  "GlobPtr", /* name of module */
+  GlobPtr_doc, /* module documentation, may be NULL */
+  -1, /* size of per-interpreter state of the module,
+       *  or -1 if the module keeps state in global variables. */
+  GlobPtrMethods
+};
+
+PyMODINIT_FUNC
+PyInit_GlobPtr(void)
+{
+    return PyModule_Create(&GlobPtrmodule);
+}
+
+    
