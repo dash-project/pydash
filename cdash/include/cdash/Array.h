4ab338b647487b75cd2e2d61d23519d4b0851804
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-20 12:36:17 +0200

    Update Array.h

--- cdash/include/cdash/Array.h
+++ cdash/include/cdash/Array.h
@@ -9,8 +9,18 @@ typedef int    size_type;
 typedef int    index_type;
 
 
-CArray cdash__array__new(size_type nelem, ...);
-void   cdash__array__delete(CArray  _this);
-View   cdash__array__block(index_type block_gindex):
+CArray         cdash__array__new(size_type nelem, ...);
+void           cdash__array__delete(CArray  _this);
+View           cdash__array__block(index_type block_gindex);
+const_pointer  cdash__array__data();
+iterator       cdash__array__begin();
+const_iterator cdash__array__begin()
+iterator       cdash__array__end()
+const_iterator cdash__array__end()
+ElementType *  cdash__array__lbegin();
+ElementType *  cdash__array__lend();
+reference      cdash__array__at(size_type global_pos);
+
+
 
 #endif /* PYDASH__ARRAY_H__INCLUDED */
