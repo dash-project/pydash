7cb6bafc008143a10c11eb01daf186768dd2c14d
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-20 13:54:58 +0200

    Update Array.h
    Added more methods

--- cdash/include/cdash/Array.h
+++ cdash/include/cdash/Array.h
@@ -3,24 +3,37 @@
 
 #include <libdash.h>
 
-
 typedef void * CArray;
 typedef int    size_type;
 typedef int    index_type;
+typedef int    bool;
+typedef int    level;
+typedef self_t;
+typedef dash::HView HView;
 
 
-CArray         cdash__array__new(size_type nelem, ...);
-void           cdash__array__delete(CArray  _this);
-View           cdash__array__block(index_type block_gindex);
-const_pointer  cdash__array__data();
-iterator       cdash__array__begin();
-const_iterator cdash__array__begin()
-iterator       cdash__array__end()
-const_iterator cdash__array__end()
-ElementType *  cdash__array__lbegin();
-ElementType *  cdash__array__lend();
-reference      cdash__array__at(size_type global_pos);
-
-
+CArray                cdash__array__new(size_type nelem, ...);
+void                 cdash__array__delete(CArray  _this);
+View                 cdash__array__block(index_type block_gindex);
+const_pointer        cdash__array__data();
+iterator             cdash__array__begin();
+const_iterator       cdash__array__begin();
+iterator             cdash__array__end();
+const_iterator       cdash__array__end();
+ElementType *        cdash__array__lbegin();
+ElementType *        cdash__array__lend();
+reference            cdash__array__at(size_type global_pos);
+const_reference      cdash__array__at(size_type global_pos);
+size_type            cdash__array__size();
+size_type            cdash__array__capacity();
+Team &               cdash__array__team();
+size_type            cdash__array__lsize();
+size_type            cdash__array__lcapacity();
+bool                 cdash__array__empty();
+View                 cdash__array__local_in(dash::util::Locality::Scope scope);
+bool                 cdash__array__is_local(index_type global_index);
+void                 cdash__array__barrier();
+const PatternType &  cdash__array__pattern();
+HView<self_t, level> cdash__array__hview();
 
 #endif /* PYDASH__ARRAY_H__INCLUDED */
