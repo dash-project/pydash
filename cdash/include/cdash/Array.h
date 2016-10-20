10bd81ee8e226c661c823723e0b348c03a219ff4
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-20 14:11:32 +0200

    Update Array.h

--- cdash/include/cdash/Array.h
+++ cdash/include/cdash/Array.h
@@ -12,33 +12,63 @@ typedef             self_t;
 typedef dash::HView HView;
 
 
-CArray               cdash__array__new(size_type nelem, ...);
-void                 cdash__array__delete(CArray  _this);
+CArray               cdash__array__new(CArray _this, 
+                                       size_type nelem, ...);
+
+void                 cdash__array__delete(CArray _this);
+
 View                 cdash__array__block(index_type block_gindex);
-const_pointer        cdash__array__data();
-iterator             cdash__array__begin();
-const_iterator       cdash__array__begin();
-iterator             cdash__array__end();
-const_iterator       cdash__array__end();
-ElementType *        cdash__array__lbegin();
-ElementType *        cdash__array__lend();
+
+const_pointer        cdash__array__data(CArray _this);
+
+iterator             cdash__array__begin(CArray _this);
+
+const_iterator       cdash__array__begin(CArray _this);
+
+iterator             cdash__array__end(CArray _this);
+
+const_iterator       cdash__array__end(CArray _this);
+
+ElementType *        cdash__array__lbegin(CArray _this);
+
+ElementType *        cdash__array__lend(CArray _this);
+
 reference            cdash__array__at(size_type global_pos);
+
 const_reference      cdash__array__at(size_type global_pos);
-size_type            cdash__array__size();
-size_type            cdash__array__capacity();
-Team &               cdash__array__team();
-size_type            cdash__array__lsize();
-size_type            cdash__array__lcapacity();
-bool                 cdash__array__empty();
-View                 cdash__array__local_in(dash::util::Locality::Scope scope);
-bool                 cdash__array__is_local(index_type global_index);
-void                 cdash__array__barrier();
-const PatternType &  cdash__array__pattern();
-HView<self_t, level> cdash__array__hview();
-bool                 cdash__array__allocate(size_type nelem, 
+
+size_type            cdash__array__size(CArray _this);
+
+size_type            cdash__array__capacity(CArray _this);
+
+Team &               cdash__array__team(CArray _this);
+
+size_type            cdash__array__lsize(CArray _this);
+
+size_type            cdash__array__lcapacity(CArray _this);
+
+bool                 cdash__array__empty(CArray _this);
+
+View                 cdash__array__local_in(CArray _this, 
+                                            dash::util::Locality::Scope scope);
+
+bool                 cdash__array__is_local(CArray _this, 
+                                            index_type global_index);
+
+void                 cdash__array__barrier(CArray _this);
+
+const PatternType &  cdash__array__pattern(CArray _this);
+
+HView<self_t, level> cdash__array__hview(CArray _this);
+
+bool                 cdash__array__allocate(CArray _this,
+                                            size_type nelem, 
                                             dash::DistributionSpec<1> distribution,
                                             dash::Team &              team = dash::Team::All());
-void                 cdash__array__deallocate();
-bool                 cdash__array__allocate(const PatternType & pattern);
+
+void                 cdash__array__deallocate(CArray _this);
+
+bool                 cdash__array__allocate(CArray _this,
+                                            const PatternType & pattern);
 
 #endif /* PYDASH__ARRAY_H__INCLUDED */
