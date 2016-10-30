223ba7bafe3d8621ceaf38fd82202d29e740793a
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-30 15:08:24 +0100

    Update Array.cc

--- cdash/src/Array.cc
+++ cdash/src/Array.cc
@@ -149,3 +149,174 @@ void                  cdash__array__AsyncArrayRef_fetch(CAsyncArrayRef _this)
     _this.fetch();
 }
     
+/*
+ * ArrayRef (declared inside Array.h)
+ *
+ */
+CArrayRef            cdash__array__ArrayRef_init(          CArray    array)
+{
+    return reinterpret_cast<CArrayRef>ArrayRef(array);   
+}
+
+void                 cdash__array__ArrayRef_delete(        CArrayRef _this)
+{
+    ~ArrayRef(_this);
+}
+
+
+Team              &  cdash__array__ArrayRef_team(          CArrayRef _this)
+{
+    return reinterpret_cast<Team>ArrayRef(_this);
+}
+
+size_type            cdash__array__ArrayRef_size(          CArrayRef _this)
+{
+    return reinterpret_cast<size_type>_this.size();
+}
+
+size_type            cdash__array__ArrayRef_local_size(    CArrayRef _this)
+{
+    return reinterpret_cast<size_type>_this.local_size();    
+}
+
+size_type            cdash__array__ArrayRef_local_capacity(CArrayRef _this)
+{
+    return reinterpret_cast<size_type>_this.local_capacity();
+}
+
+size_type            cdash__array__ArrayRef_extent(CArrayRef _this,       
+                                  dim_t     dim)
+{
+    
+}
+
+Extents_t            cdash__array__ArrayRef_extents(       CArrayRef _this)
+{
+    
+}
+
+bool                 cdash__array__ArrayRef_empty(         CArrayRef _this)
+{
+    
+}
+
+void                 cdash__array__ArrayRef_barrier(       CArrayRef _this)
+{
+    
+}
+
+const_pointer        cdash__array__ArrayRef_data(          CArrayRef _this)
+{
+    
+}
+
+iterator             cdash__array__ArrayRef_begin(         CArrayRef _this)
+{
+    
+}
+
+const_iterator       cdash__array__ArrayRef_begin(         CArrayRef _this)
+{
+    
+}
+
+iterator             cdash__array__ArrayRef_end(           CArrayRef _this)
+{
+    
+}
+
+const_iterator       cdash__array__ArrayRef_end(           CArrayRef _this)
+{
+    
+}
+
+local_type           cdash__array__ArrayRef_sub_local(     CArrayRef _this)
+{
+    
+}
+
+ElementType       *  cdash__array__ArrayRef_lbegin(        CArrayRef _this)
+{
+    
+}
+    
+ElementType       *  cdash__array__ArrayRef_lend(          CArrayRef _this)
+{
+    
+}
+
+reference            cdash__array__ArrayRef_random_access( size_type global_index)
+{
+    
+}
+    
+const_reference      cdash__array__ArrayRef_random_access( size_type global_index)
+{
+    
+}
+    
+reference            cdash__array__ArrayRef_at(            CArrayRef _this, 
+                                                           size_t    n)
+{
+    
+}
+                                                               
+const_reference      cdash__array__ArrayRef_at(            CArrayRef _this, 
+                                                           size_t    n)
+                                                               
+                                                           {
+    
+                                                           }
+                                                               
+PatternType &        cdash__array__ArrayRef_pattern(       CArrayRef _this)
+{
+    
+}
+    
+          
+          
+/*
+ * A distributed array.
+ */
+
+typedef dash::util::Locality::Scope Scope;
+
+local_type  cdash__array_local;
+async_type  cdash__array_async;
+
+CArray cdash__array__new(/*param list*/);
+void   cdash__array__delete(CArray _this);
+View                 cdash__array__block(index_type block_gindex);
+
+const_pointer        cdash__array__data(         CArray     _this);
+iterator             cdash__array__begin(        CArray     _this);
+const_iterator       cdash__array__begin(        CArray     _this);
+iterator             cdash__array__end(          CArray     _this);
+const_iterator       cdash__array__end(          CArray     _this);
+ElementType *        cdash__array__lbegin(       CArray     _this);
+ElementType *        cdash__array__lend(         CArray     _this);
+reference            cdash__array__random_access(size_type  global_index);
+const_reference      cdash__array__random_access(size_type  global_index);
+reference            cdash__array__at(           size_type  global_pos);
+const_reference      cdash__array__at(           size_type  global_pos);
+size_type            cdash__array__size(         CArray     _this);
+size_type            cdash__array__capacity(     CArray     _this);
+Team &               cdash__array__team(         CArray     _this);
+size_type            cdash__array__lsize(        CArray     _this);
+size_type            cdash__array__lcapacity(    CArray     _this);
+bool                 cdash__array__empty(        CArray     _this);
+View                 cdash__array__local_in(     CArray     _this, 
+                                                 Scope      scope);
+bool                 cdash__array__is_local(     CArray     _this, 
+                                                 index_type global_index);
+void                 cdash__array__barrier(      CArray     _this);
+const PatternType &  cdash__array__pattern(      CArray     _this);
+HView<self_t, level> cdash__array__hview(        CArray    _this);
+bool                 cdash__array__allocate(     CArray    _this,
+                                                 size_type nelem, 
+                                                 DistrSpec distribution,
+                                                 Team &    team);
+void                 cdash__array__deallocate(   CArray    _this);
+
+bool                 cdash__array__allocate(CArray         _this,
+                                            PatternType &  pattern);
