70a7060fe7735735f47c8c09a92b4709f0f7be33
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-30 14:51:12 +0100

    Update Array.cc

--- cdash/src/Array.cc
+++ cdash/src/Array.cc
@@ -70,3 +70,82 @@ self_t        cdash__array__LocalArrayRef_block(CLocalArrayRef larray,
 }
 
 PatternType & cdash__array__LocalArrayRef_pattern();
+
+/*
+ * AsyncArrayRef (declared inside Array.h)
+ *
+ */
+CAsyncArrayRef        cdash__array__AsyncArrayRef_init( CArray array)
+{
+    return reinterpret_cast<CASyncArrayRef> (AsyncArrayRef(array));
+}
+
+void                  cdash__array__AsyncArrayRef_delete(CAsyncArrayRef _this)
+{
+    ~AsyncArrayRef(_this);  
+}
+
+const_pointer         cdash__array__AsyncArrayRef_begin(CAsyncArrayRef _this)
+{
+    return reinterpret_cast<const_pointer>(_this.begin());  
+}
+
+pointer               cdash__array__AsyncArrayRef_begin(CAsyncArrayRef _this)
+{
+    return reinterpret_cast<pointer>(_this.begin());  
+}
+
+const_pointer         cdash__array__AsyncArrayRef_end(CAsyncArrayRef _this)
+{
+    return reinterpret_cast<const_pointer>(_this.end());  
+}
+pointer               cdash__array__AsyncArrayRef_end(CAsyncArrayRef _this)
+{
+    return reinterpret_cast<pointer>(_this.end());  
+}
+
+size_type             cdash__array__AsyncArrayRef_size(CAsyncArrayRef _this)
+{
+    return reinterpret_cast<size_type>(_this.size());  
+}
+
+const_async_reference cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this, size_t n)
+{
+    return reinterpret_cast<const_async_reference>(_this[n]);  
+}
+
+async_reference       cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this, size_t n)
+{
+    return reinterpret_cast<async_reference>(_this[n]);  
+}
+
+void                  cdash__array__AsyncArrayRef_flush(CAsyncArrayRef _this)
+{
+    _this.flush();
+}
+
+void                  cdash__array__AsyncArrayRef_flush_local(CAsyncArrayRef _this);
+{
+    _this.flush();
+}
+
+void                  cdash__array__AsyncArrayRef_flush_all(CAsyncArrayRef _this)
+{
+    _this.flush_all();
+}
+    
+void                  cdash__array__AsyncArrayRef_flush_local_all(CAsyncArrayRef _this)
+{
+    _this.flush_local_all();
+}
+    
+void                  cdash__array__AsyncArrayRef_push(CAsyncArrayRef _this)
+{
+    _this.push();
+}
+    
+void                  cdash__array__AsyncArrayRef_fetch(CAsyncArrayRef _this)
+{
+    _this.fetch();
+}
+    
