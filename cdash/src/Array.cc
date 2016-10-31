38f9618f1f686b0b8b4ac900865589d6f476cc2b
Josef Schäffer <JosefSchaeffer@users.noreply.github.com>
2016-10-31 11:36:38 +0100

    Update Array.cc

--- cdash/src/Array.cc
+++ cdash/src/Array.cc
@@ -9,17 +9,17 @@
  */
 
 //LocalArrayRef
-CLocalArrayRef cdash__array__LocalArrayRef_init(        CArray         array)
+CLocalArrayRef cdash__array__LocalArrayRef_init(CArray array)
 {
   return reinterpret_cast<CLocalArrayRef>(dash::LocalArrayRef(array));
 }
 
-CLocalArrayRef cdash__array__LocalArrayRef_delete(      CCLocalArrayRef array)
+CLocalArrayRef cdash__array__LocalArrayRef_delete(CCLocalArrayRef array)
 {
  // reinterpret_cast from CLocalArrayRef to LocalArrayRef, then deallocate?
 } 
 
-const_pointer cdash__array__LocalArrayRef_begin(        CLocalArrayRef larray)
+const_pointer cdash__array__LocalArrayRef_begin(CLocalArrayRef larray)
 {
    return reinterpret_cast<const_pointer>larray.begin();
 }
@@ -108,12 +108,14 @@ size_type cdash__array__AsyncArrayRef_size(CAsyncArrayRef _this)
     return reinterpret_cast<size_type>(_this.size());  
 }
 
-const_async_reference cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this, size_t n)
+const_async_reference cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this, 
+                                                                size_t n)
 {
     return reinterpret_cast<const_async_reference>(_this[n]);  
 }
 
-async_reference cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this, size_t n)
+async_reference cdash__array__AsyncArrayRef_random_access(CAsyncArrayRef _this,
+                                                          size_t n)
 {
     return reinterpret_cast<async_reference>(_this[n]);  
 }
@@ -151,12 +153,12 @@ void cdash__array__AsyncArrayRef_fetch(CAsyncArrayRef _this)
  * ArrayRef (declared inside Array.h)
  *
  */
-CArrayRef cdash__array__ArrayRef_init( CArray    array)
+CArrayRef cdash__array__ArrayRef_init(CArray array)
 {
     return reinterpret_cast<CArrayRef>ArrayRef(array);   
 }
 
-void cdash__array__ArrayRef_delete( CArrayRef _this)
+void cdash__array__ArrayRef_delete(CArrayRef _this)
 {
     ~ArrayRef(_this);
 }
@@ -250,7 +252,8 @@ reference cdash__array__ArrayRef_random_access(CArrayRef _this,
     return reinterpret_cast<reference>(_this[global_index]);   
 }
     
-const_reference cdash__array__ArrayRef_random_access(size_type global_index)
+const_reference cdash__array__ArrayRef_random_access(CArrayRef _this,
+                                                     size_type global_index)
 {
     return reinterpret_cast<const_reference>(_this[global_index]);   
  
@@ -296,32 +299,32 @@ View cdash__array__block(CArray _this, index_type block_gindex)
   return reinterpret_cast<View>_this.block(block_gindex);
 }
 
-const_pointer        cdash__array__data(CArray _this)
+const_pointer cdash__array__data(CArray _this)
 {
   return reinterpret_cast<View>_this.data();
 }
 
-iterator             cdash__array__begin(CArray _this)
+iterator cdash__array__begin(CArray _this)
 {
   return reinterpret_cast<iterator>_this.begin();
 }
 
-const_iterator       cdash__array__begin(CArray _this)
+const_iterator cdash__array__begin(CArray _this)
 {
   return reinterpret_cast<const_iterator>_this.begin();
 }
 
-iterator             cdash__array__end(CArray _this)
+iterator cdash__array__end(CArray _this)
 {
   return reinterpret_cast<iterator>_this.end();
 }
 
-const_iterator       cdash__array__end(CArray _this)
+const_iterator cdash__array__end(CArray _this)
 {
   return reinterpret_cast<const_iterator>_this.end();
 }
 
-ElementType *        cdash__array__lbegin(CArray _this)
+ElementType * cdash__array__lbegin(CArray _this)
 {
   return reinterpret_cast<ElementType>_this.lbegin();
 }
@@ -341,17 +344,20 @@ reference cdash__array__random_access(CArray    _this,
   return reinterpret_cast<reference>(_this.[global_index]);                                     
 }
 
-const_reference cdash__array__random_access(CArray _this, size_type  global_index)
+const_reference cdash__array__random_access(CArray     _this, 
+                                            size_type  global_index)
 {
   return reinterpret_cast<const_reference>(_this.[global_index]);                                     
 }
 
-reference cdash__array__at(CArray _this, size_type global_pos)
+reference cdash__array__at(CArray    _this, 
+                           size_type global_pos)
 {
   return reinterpret_cast<reference>(_this.at(global_pos));                                     
 }
 
-const_reference cdash__array__at(CArray _this,size_type  global_pos)
+const_reference cdash__array__at(CArray     _this,
+                                 size_type  global_pos)
 {
     return reinterpret_cast<const_reference>(_this.at(global_pos));                                     
 }
@@ -376,17 +382,17 @@ size_type cdash__array__lsize(CArray _this)
     return reinterpret_cast<size_type>(_this.lsize());                                     
 }
  
-size_type cdash__array__lcapacity(    CArray     _this)
+size_type cdash__array__lcapacity(CArray     _this)
 {
     return reinterpret_cast<size_type>(_this.lcapacity());                                     
 }
  
-bool cdash__array__empty(        CArray     _this)
+bool cdash__array__empty(CArray     _this)
 {
     return _this.empty();
 }
 
-View cdash__array__local_in(     CArray     _this, 
+View cdash__array__local_in(CArray     _this, 
                                                  Scope      scope)
 {
     return reinterpret_cast<View>(_this.local_in(scope));
