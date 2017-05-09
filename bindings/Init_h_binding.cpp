7a53bf063f345e6c0425fd88854c132d6aceb5b5
Josef Schäffer <josef.schaeffer@hotmail.de>
2017-05-09 22:47:15 +0200

    Test Impl.

---
+++ bindings/Init_h_binding.cpp
@@ -0,0 +1,22 @@
+#include <pybind11/pybind11.h>
+
+#include "dash/dash/include/dash/Init.h"
+
+namespace py = pybind11;
+
+PYBIND11_PLUGIN(DASH) {
+    py::module m("dash", "DASH Binding");
+		
+		//y::class_<ec::error_class> error_class(m, "error_class");
+		
+		m.def("init",							&dash::init);
+		m.def("finalize", 				&dash::finalize);
+		m.def("is_initialized", 	&dash::is_initialized);
+		m.def("is_multithreaded", &dash::is_multithreaded);
+		m.def("myid", 						&dash::myid);
+		m.def("size", 						&dash::size);
+		m.def("barrier", 					&dash::barrier);
+				
+		return m.ptr();
+		
+}
