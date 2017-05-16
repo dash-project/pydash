51345a270e31f62a4a8aa34648241304ce446faf
Tobias Fuchs <twh.fuchs@gmail.com>
2017-05-16 17:12:48 +0000

    Fixed syntax

--- bindings/Init_h_binding.cpp
+++ bindings/Init_h_binding.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 
-#include "dash/dash/include/dash/Init.h"
+#include "~/workspaces/dash/dash/include/dash/Init.h"
 
 namespace py = pybind11;
 
@@ -10,10 +10,10 @@ PYBIND11_PLUGIN(DASH) {
 		m.def("init",             (void  (*)(int*, char***)) &dash::init,              "Initalize DASH runtime");
 		m.def("finalize", 				(void)                     &dash::finalize,          "Finalize the DASH library and the underlying runtime system.");
 		m.def("is_initialized", 	(bool)                     &dash::is_initialized,    "Check whether DASH has been initialized already.");
-		m.def("is_multithreaded", (bool)                     &dash::is_multithreaded), "Check whether DASH has been initialized with support for multi-threaded access.");
-		m.def("myid", 						(global_unit_t)            &dash::myid,               "Shortcut to query the global unit ID of the calling unit.");
-		m.def("size", 						(ssize_t)                  &dash::size,               "Return the number of units in the global team.");
-		m.def("barrier", 					(void)                     &dash::barrier,            "A global barrier involving all units.");
+		m.def("is_multithreaded", (bool)                     &dash::is_multithreaded,  "Check whether DASH has been initialized with support for multi-threaded access.");
+		m.def("myid", 						(global_unit_t)            &dash::myid,              "Shortcut to query the global unit ID of the calling unit.");
+		m.def("size", 						(ssize_t)                  &dash::size,              "Return the number of units in the global team.");
+		m.def("barrier", 					(void)                     &dash::barrier,           "A global barrier involving all units.");
 				
 		return m.ptr();
 		
