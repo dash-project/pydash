f4f1c198746dc481d610aaa7b18eb1ebdbe0d112
Tobias Fuchs <twh.fuchs@gmail.com>
2017-05-17 02:26:47 +0000

    dash::Array polish read/write example

--- test/array_test.py
+++ test/array_test.py
@@ -1,31 +1,24 @@
-
 import pydash
 
 pydash.initialize(0, "")
 
 myid   = pydash.myid().id()
 nunits = pydash.nunits()
-
 # Collectively instantiate array:
-arr = pydash.ArrayInt(3 * nunits)
-
+array  = pydash.ArrayInt(3 * nunits)
 # Initialize array:
-arr.set(myid * 3 + 0, 100 * (1 + myid) + 0)
-arr.set(myid * 3 + 1, 100 * (1 + myid) + 1)
-arr.set(myid * 3 + 2, 100 * (1 + myid) + 2)
-
+array.set(myid * 3 + 0, 100 * (1 + myid) + 0)
+array.set(myid * 3 + 1, 100 * (1 + myid) + 1)
+array.set(myid * 3 + 2, 100 * (1 + myid) + 2)
 # Wait for all units:
 pydash.barrier()
-
+# Read array elements from remote unit:
 fromid = nunits - myid - 1
 
-print("Unit {} read array[{}..{}] : {}, {}, {}".format(
-      myid,
-      fromid * 3,
-      fromid * 3 + 2,
-      arr.get(fromid * 3 + 0),
-      arr.get(fromid * 3 + 1),
-      arr.get(fromid * 3 + 2)
-     ))
+print("Unit {0:2} read array[{1:3} ..{2:3}]:"
+        .format(myid, fromid * 3, fromid * 3 + 2),
+      repr(array.get(fromid * 3 + 0)).rjust(4),
+      repr(array.get(fromid * 3 + 1)).rjust(4),
+      repr(array.get(fromid * 3 + 2)).rjust(4))
 
 pydash.finalize()
