f52591c6c1a781da628790faf43258ab99fd14f5
Josef Schäffer <josef.schaeffer@hotmail.de>
2017-05-23 14:47:16 +0200

    Added Test for Return Value Policies

--- test/test_value_categories/test_value_categories.py
+++ test/test_value_categories/test_value_categories.py
@@ -9,7 +9,30 @@ v_0 = pydash.LV(0, "zero  ")
 v_1 = pydash.LV(1, "first ")
 #v_2 = pydash.LV(2, "second")
 
-v_0 = v_1 #copy or reference?
+v_0 = v_18
 
+a = pydash.LV(123, "A");
+
+print("main -------------------------------------");
+                
+b = pydash.LV(455, "B")
+
+print("main -------------------------------------");
+
+a_2 = pydash.LV(a)
+    
+print("main -------------------------------------");
+
+e = pydash.LV((pydash.LV(130, "E"))
+e.value(50)
+
+print("main -------------------------------------");
+
+m = pydash.ret_logged_val_by_val(145, "M")
+
+print("main -----*PFWOOOHFFF*--------------------")
+              
+p = pydash.acc_logged_val_by_val(pydash.ret_logged_val_by_val(234, "X"))
+print(p.value(), p.name())
 
 pydash.finalize()
