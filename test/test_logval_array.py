fd6602f30b389850cde39aa8661ec7745ce574c1
Josef Schäffer <josef.schaeffer@hotmail.de>
2017-05-23 10:30:47 +0200

    Changed Binding Order

--- test/test_logval_array.py
+++ test/test_logval_array.py
@@ -4,14 +4,18 @@ pydash.initialize(0, "")
 
 myid   = pydash.myid().id()
 nunits = pydash.nunits()
+
 # Collectively instantiate array:
 array  = pydash.ArrayLV(3 * nunits)
+
 # Initialize array:
-array[myid * 3 + 0] = { "first",  myid };
-array[myid * 3 + 1] = { "second", myid };
-array[myid * 3 + 2] = { "third",  myid };
+array[myid * 3 + 0] = pydash.LV(myid, "first")
+array[myid * 3 + 1] = pydash.LV(myid, "second")
+array[myid * 3 + 2] = pydash.LV(myid, "third")
+
 # Wait for all units:
 pydash.barrier()
+
 # Read array elements from remote unit:
 fromid = nunits - myid - 1
 
