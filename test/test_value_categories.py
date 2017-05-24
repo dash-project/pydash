2b5b6b809093035fb216785a522b7be71c4946ab
Josef Schäffer <josef.schaeffer@hotmail.de>
2017-05-24 10:02:53 +0200

    Adaption to new Interface of Logged_Val

--- test/test_value_categories.py
+++ test/test_value_categories.py
@@ -13,22 +13,22 @@ pydash.initialize(0, "")
 #v_0 = v_1
 
 
-a   = pydash.LV(123, "A");
+a   = pydash.LV(123, 'A');
 a.set_value(10)
 
-b   = pydash.LV(455, "B")
+b   = pydash.LV(455, 'B')
 b.set_value(20)
 
 a_2 = pydash.LV(a)
 a_2.set_value(30)
 
-e   = pydash.LV(pydash.LV(130, "E"))
+e   = pydash.LV(pydash.LV(130, 'E'))
 e.set_value(50)
 
-m   = pydash.ret_logged_val_by_val(145, "M")
+m   = pydash.ret_logged_val_by_val(145, 'M')
 m.set_value(40)
 
-p   = pydash.acc_logged_val_by_val(pydash.ret_logged_val_by_val(234, "X"))
+p   = pydash.acc_logged_val_by_val(pydash.ret_logged_val_by_val(234, 'X'))
 print(p)
 
 pydash.finalize()
