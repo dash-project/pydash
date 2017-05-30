0b9182a79ddfec49bf35e857fb651d51220b929e
Tobias Fuchs <twh.fuchs@gmail.com>
2017-05-30 00:08:42 +0000

    Added binding of dash::Array.local

--- test/test_array_local.py
+++ test/test_array_local.py
@@ -7,18 +7,20 @@ myid   = pydash.myid().id()
 nunits = pydash.nunits()
 
 # Collectively instantiate array:
-array  = pydash.ArrayInt(3 * nunits)
+array  = pydash.ArrayLV(3 * nunits)
 
 # Initialize array:
 for l in range(0,array.local.size()):
-    array.local[l] = 100 * (1 + myid) + l
+    array.local[l] = pydash.LV(100 * (1 + myid) + l, 'A')
 
 pydash.barrier()
 
 for u in range(0,nunits):
     if u == myid:
+        print("unit {} Array.local: {}".format(myid, array.local))
         for lelem in array.local:
-            print("unit {} Array.local[]: {}".format(myid, lelem))
+            print("unit {} Array.local[]: {}-{}"
+                    .format(myid, lelem.value(), lelem.name()))
     pydash.barrier()
 
 time.sleep(1)
@@ -26,7 +28,8 @@ pydash.barrier()
 
 if myid == 0:
     for gi in range(0,array.size()):
-        print("Array[{}]: {}".format(gi, array[gi]))
+        print("Array[{}]: {}-{}"
+                .format(gi, array[gi].value(), array[gi].name()))
 
 pydash.barrier()
 
